80926345121a19cfa3eb8bd4d4f708971bbc0996
Lab Incorporation <dev.frolics@gmail.com>
2023-03-09 12:51:09 +0530

    Gallery is finally here stable
    Squashed some bugs in search page, removed more clutter from computer_course.html and
    and GALLERY is there
    
    Images used are used for educational purposes. We respect the IP rights of original creators.
    If your creation/images are used in accident, you can raise a issue request to get it removed.
    
    Thanks to @Manish and @Krishna

--- script.js
+++ script.js
@@ -76,44 +76,7 @@ function goFurther(){
   }
 }
 
-// function google() {
-//   	window.location.assign("https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&csig=AF-SEnbZHbi77CbAiuHE%3A1585466693&flowName=GlifWebSignIn&flowEntry=AddSession", "_blank");
-// }
 
-// QUIZ Page
-function quizt(frame) {
-  document.getElementById('f1').style='display: none;';
-  document.getElementById('f2').style='display: none;';
-  document.getElementById('f3').style='display: none;';
-  document.getElementById('f4').style='display: none;';
-  document.getElementById('f5').style='display: none;';
-  document.getElementById('f6').style='display: none;';
-  document.getElementById('f7').style='display: none;';
-  document.getElementById('f8').style='display: none;';
-  document.getElementById('f9').style='display: none;';
-  document.getElementById('f10').style='display: none;';
-  document.getElementById('f11').style='display: none;';
-  if(frame == 1) document.getElementById('f1').style = 'display: block';
-  else if(frame == 2) document.getElementById('f2').style = 'display: block';
-  else if(frame == 3) document.getElementById('f3').style = 'display: block';
-  else if(frame == 4) document.getElementById('f4').style = 'display: block';
-  else if(frame == 5) document.getElementById('f5').style = 'display: block';
-  else if(frame == 6) document.getElementById('f6').style = 'display: block';
-  else if(frame == 7) document.getElementById('f7').style = 'display: block';
-  else if(frame == 8) document.getElementById('f8').style = 'display: block';
-  else if(frame == 9) document.getElementById('f9').style = 'display: block';
-  else if(frame == 10) document.getElementById('f10').style = 'display: block';
-  else if(frame == 11) document.getElementById('f11').style = 'display: block'; 
-  else alert('error');
-}
-
-function startquiz() {
-  document.getElementById('title').style = 'display: none;'; 
-
-  document.getElementById('panel').style = 'display: inline-flex;'; 
-  document.getElementById('left').style = 'display: block;'; 
-  document.getElementById('right').style = 'display: block;'; 
-}
 function searchdisplay() {
   document.getElementById('searchpanel').style.display="block";
 }
