a45ff68ff092b71ea883f2b3629de3d445fd913f
Lab Rat <dev.frolics@gmail.com>
2023-05-26 22:00:34 +0530

    Fixed margin in Contact Us page

--- script.js
+++ script.js
@@ -12,6 +12,7 @@
 //   $('.form-box').hide();  
 //  });
 
+
 function scrollAppear() {
   var introText = document.querySelector('.side-text');
   var sideImage = document.querySelector('.sideImage');
@@ -123,10 +124,11 @@ function display(n) {
 function sideMenu(side) {
   var menu = document.getElementById('side-menu');
   if(side==0) {
+    menu.style.visibility = "visible";
     menu.style = 'transition: .8s; transform: translateX(0vh); position:fixed;';
   }
   else {
-    menu.style = 'transition: .3s; transform: translateX(-100%);';
+    menu.style = 'transform: translateX(-100%);';
   }
   side++;
 }
